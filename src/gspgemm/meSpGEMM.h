a0b3a0a144b7e43331a83c796519a9ea9a245e72
Oguz Selvitopi <roselvitopi@lbl.gov>
2021-03-29 00:30:40 -0400

    Fix bug when per-process-mem is not provided

--- src/gspgemm/meSpGEMM.h
+++ src/gspgemm/meSpGEMM.h
@@ -447,25 +447,45 @@ MemEfficientSpGEMMg (
 	}
 	flops /= stages;
 	cf	  /= stages;
-	if (local_spgemm == LSPG_HYBRID)
-	{
-		local_spgemm = LSPG_NSPARSE;
-		if (flops / phases < (2 * 1e6))
-			local_spgemm = LSPG_CPU;
-		else if (cf <= 3)
+	if (perProcessMemory == 0)
+ 	{
+		if (local_spgemm == LSPG_HYBRID)
 		{
-			// local_spgemm = LSPG_RMERGE2;
-			local_spgemm = LSPG_CPU;
+			if (iter < 10)
+				local_spgemm = LSPG_NSPARSE;
+			else
+				local_spgemm = LSPG_CPU;
 		}
-		
-		#ifdef LOG_GNRL_ME_SPGEMM
-		lfile << "local hybrid spgemm " <<
-			(local_spgemm == LSPG_CPU ? "cpu" :
-			 (local_spgemm == LSPG_RMERGE2 ? "gpu_rmerge2" :
-			  (local_spgemm == LSPG_NSPARSE ? "gpu_nsparse" : "N/A")))
-			  << std::endl;
-		#endif
 	}
+	else
+	{
+		if (local_spgemm == LSPG_HYBRID)
+		{
+			local_spgemm = LSPG_NSPARSE;
+			if (flops / phases < (2 * 1e6))
+				local_spgemm = LSPG_CPU;
+			else if (cf <= 3)
+			{
+				// local_spgemm = LSPG_RMERGE2;
+				local_spgemm = LSPG_CPU;
+			}
+
+			#ifdef LOG_GNRL_ME_SPGEMM
+			lfile << "local hybrid spgemm " <<
+				(local_spgemm == LSPG_CPU ? "cpu" :
+				 (local_spgemm == LSPG_RMERGE2 ? "gpu_rmerge2" :
+				  (local_spgemm == LSPG_NSPARSE ? "gpu_nsparse" : "N/A")))
+				  << std::endl;
+			#endif
+		}
+	}
+
+	if (myrank == 0)
+		std::cout << "local hybrid spgemm at iter " << iter << ": " <<
+				(local_spgemm == LSPG_CPU ? "cpu" :
+				 (local_spgemm == LSPG_RMERGE2 ? "gpu_rmerge2" :
+				  (local_spgemm == LSPG_NSPARSE ? "gpu_nsparse" : "N/A")))
+				  << std::endl;
 
 	LIA C_m = A.seqptr()->getnrow();
     LIB C_n = B.seqptr()->getncol();
